e2fdbcd087260c1450ac50f7d9098ff9ead581c9
christiaannnshi <migueljameschristian@gmail.com>
2026-05-04 13:32:52 +0800

    Restore docs build for GitHub Pages

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiBaseUrl: 'http://localhost/eccms-api',
-  // Replace with your Google Cloud OAuth 2.0 Client ID
-  // https://console.cloud.google.com/apis/credentials
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID'
-};
